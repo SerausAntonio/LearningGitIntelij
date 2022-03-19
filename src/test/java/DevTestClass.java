e8eb0087325bb572d9eafdebe6bc0d6ebcfde8c5
AntonioSeraus <aseraus@hotmail.com>
2022-03-19 23:13:07 +0100

    Merge branch 'master' into feature2.0
    # Conflicts:
    #	src/test/java/DevTestClass.java

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -3,9 +3,10 @@ public class DevTestClass {
     public static void main(String[] args){
         System.out.println("DevTestClass!!!");
         //Antonio added this!!!
+        bla();
     }
 //This is a commit
-    public void bla(){
+    public static void bla(){
         System.out.println("Bla bla bla");
     }
 }
